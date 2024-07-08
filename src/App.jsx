96af265d9272233eb5297897e180423c7d2b076a
BernardoLangmachado <130462210+BernardoLangmachado@users.noreply.github.com>
2024-07-08 10:20:53 -0300

    Prova

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import Times from '../src/components/BuscarTIME'
+export default function App() {
+
+  return (
+    <>
+     <Header/>  
+    <Times/>
+    </>
+   
+  )
+}
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className="Header">
+            <img className="imgsenai" src="https://logodownload.org/wp-content/uploads/2019/08/senai-logo-1.png"></img>
+            <p className="nome">3C Bernardo L</p>
+        </div>
+    )
+}
